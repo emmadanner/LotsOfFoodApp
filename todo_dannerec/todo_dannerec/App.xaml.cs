c0b1fb4cd39758d25bce054522d03e7da6340430
emmadanner <edanneruc@gmail.com>
2024-03-18 12:55:40 -0400

    Add project files.

---
+++ todo_dannerec/todo_dannerec/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using todo_dannerec.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using todo_dannerec.Views;
+using todo_dannerec.Data;
+
+namespace todo_dannerec
+{
+    public partial class App : Application
+    {
+        private static TodoItemDatabase database;
+        internal static TodoItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new TodoItemDatabase(Path.Combine(Environment.GetFolderPath
+                    (Environment.SpecialFolder.LocalApplicationData), "TodoListItems.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new TodoListPage());
+        }
+        protected override void OnStart()
+        {
+        }
+        protected override void OnSleep()
+        {
+        }
+        protected override void OnResume()
+        {
+        }
+    }
+}
