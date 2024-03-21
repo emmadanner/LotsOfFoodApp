96ec94cb26369e7e700a514c9268742fa82e4485
emmadanner <edanneruc@gmail.com>
2024-03-21 02:37:03 -0400

    Renamed a few more things, errors still appearing but I plan to work on this some more.

--- todo_dannerec/todo_dannerec/Views/FoodItemPage.cs
+++ todo_dannerec/todo_dannerec/Views/FoodItemPage.cs
@@ -20,14 +20,13 @@ namespace LotsOfFoodApp.Views
         async void OnSaveClicked(object sender, EventArgs e)
         {
             var todoItem = (FoodItem)BindingContext;
-            // TodoItemDatabase database = await TodoItemDatabase.Instance;
             await App.Database.SaveItemAsync(todoItem);
             await Navigation.PopAsync();
         }
         async void OnDeleteClicked(object sender, EventArgs e)
         {
-            var todoItem = (FoodItem)BindingContext;
-            await App.Database.DeleteItemAsync(todoItem);
+            var foodItem = (FoodItem)BindingContext;
+            await App.Database.DeleteItemAsync(foodItem);
             await Navigation.PopAsync();
         }
         async void OnCancelClicked(object sender, EventArgs e)
